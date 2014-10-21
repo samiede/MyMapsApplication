2f22948556288f0c493cd7f18a8b5b693bc9f210
MoritzD <doetterl.moritz@gmail.com>
2014-10-21 22:37:16 +0000

    Test murks

--- app/src/main/java/com/ru/testapp/mymapsapplication/app/MapsActivity.java
+++ app/src/main/java/com/ru/testapp/mymapsapplication/app/MapsActivity.java
@@ -1,29 +1,158 @@
 package com.ru.testapp.mymapsapplication.app;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
+import android.util.Log;
+import android.widget.Toast;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationStatusCodes;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener{
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    public LocationClient mLocationClient;
+
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        // MapView mapView = (MapView) findViewById(R.id.map);
         setUpMapIfNeeded();
+        if(mMap!=null) {
+            // mMap.clear();
+            // mMap.setLocationSource(LocationRequest.PRIORITY_HIGH_ACCURACY);
+         /*   mMap.setLocationSource(new LocationSource() {
+                @Override
+                public void activate(OnLocationChangedListener onLocationChangedListener) {
+
+                }
+
+                @Override
+                public void deactivate() {
+
+                }
+            });*/
+            mMap.setBuildingsEnabled(true);
+            mMap.setMyLocationEnabled(true);
+            //mMap.moveCamera(new CameraUpdate());
+
+            mLocationClient = new LocationClient(this, this, this);
+
+            // Connect the client.
+            if(isGooglePlayServicesAvailable()){
+                mLocationClient.connect();
+            }
+            /*
+            Location location = mLocationClient.getLastLocation();
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
+            mMap.animateCamera(cameraUpdate);
+           // mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+           */
+        }
     }
+
+
+    /*
+ * Called when the Activity becomes visible.
+ */
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+
+    }
+
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
     }
+
+    /*
+ * Called when the Activity is no longer visible.
+ */
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+    /*
+     * Handle results returned to the FragmentActivity
+     * by Google Play services
+     */
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        // Decide what to do based on the original request code
+        switch (requestCode) {
+
+            case CONNECTION_FAILURE_RESOLUTION_REQUEST:
+            /*
+             * If the result code is Activity.RESULT_OK, try
+             * to connect again
+             */
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        mLocationClient.connect();
+                        break;
+                }
+
+        }
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        // Check that Google Play services is available
+        int resultCode =  GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Location Updates", "Google Play services is available.");
+            return true;
+        } else {
+           /* // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog( resultCode,
+                    this,
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null) {
+                // Create a new DialogFragment for the error dialog
+                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+                errorFragment.setDialog(errorDialog);
+                errorFragment.show(getSupportFragmentManager(), "Location Updates");
+            }
+              */
+            return false;
+        }
+    }
+
 
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
@@ -60,6 +189,58 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // Display the connection status
+        Toast.makeText(this, "Got Location", Toast.LENGTH_SHORT).show();
+        Location location = mLocationClient.getLastLocation();
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+        mMap.animateCamera(cameraUpdate);
+
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Possition: " + latLng + " Dragable ").draggable(true));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude()+0.01,location.getLongitude())).title("Possition: "+(location.getLatitude()+0.01)+" ; "+location.getLongitude()+" Flat ").flat(true));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude()-0.01,location.getLongitude())).title("Possition: "+(location.getLatitude()-0.01)+" ; "+location.getLongitude()+" Not Flat ").flat(false));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),location.getLongitude()+0.01)).title("Possition: "+location.getLatitude()+" ; "+(location.getLongitude()+0.01)+" Alpha: 0.5 ").alpha(0.5f));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),location.getLongitude()-0.01)).title("Possition: "+location.getLatitude()+" ; "+(location.getLongitude()-0.01)+" snippet").snippet("Test Snippet"));
+        //mMap.setPadding(0,10,0,20);
+        mMap.addCircle(new CircleOptions().center(latLng).fillColor(1996519287).radius(1000.0).strokeColor(Color.BLUE).strokeWidth(10.0f));
+    }
+
+    @Override
+    public void onDisconnected() {
+        // Display the connection status
+        Toast.makeText(this, "Disconnected. Please re-connect.",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+    /*
+     * Google Play services can resolve some errors it detects.
+     * If the error has a resolution, try sending an Intent to
+     * start a Google Play services activity that can resolve
+     * error.
+     */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            /*
+            * Thrown if Google Play services canceled the original
+            * PendingIntent
+            */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Sorry. Location services not available to you", Toast.LENGTH_LONG).show();
+        }
     }
 }
